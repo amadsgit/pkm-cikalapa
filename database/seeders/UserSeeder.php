0778573ce35b4032229d8a3d9f7f2ce9b3d455ba
Mamad Ahmad <mamadahmadfnc@gmail.com>
2026-02-18 22:07:52 +0700

    update seeder database awal user

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -4,33 +4,60 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
 
 class UserSeeder extends Seeder
 {
     public function run(): void
     {
-        DB::table('users')->insert([
+
+        $users = [
 
             [
+
                 'id' => 1,
                 'pegawai_id' => 1,
                 'nip' => '198809052020121005',
+
                 'email' => 'kapus@gmail.com',
-                'password' => bcrypt('12345678'),
+
+                'password' => Hash::make('12345678'),
+
                 'created_at' => now(),
                 'updated_at' => now(),
+
             ],
 
             [
+
                 'id' => 2,
-                'pegawai_id' => 1,
+
+                'pegawai_id' => 2,
+
                 'nip' => '198112242010012014',
+
                 'email' => 'ruhandi58@gmail.com',
-                'password' => bcrypt('12345678'),
+
+                'password' => Hash::make('12345678'),
+
                 'created_at' => now(),
                 'updated_at' => now(),
+
             ],
 
-        ]);
+        ];
+
+        foreach ($users as $user) {
+
+            DB::table('users')->updateOrInsert(
+
+                ['email' => $user['email']],
+
+                $user
+
+            );
+
+        }
+
     }
 }
