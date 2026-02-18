9a971108ab71b12ca239f0c6f520a16787b0c6ec
Mamad Ahmad <mamadahmadfnc@gmail.com>
2026-02-19 04:28:33 +0700

    update add slug to kategori_layanan

--- database/migrations/2025_10_24_140629_create_kategori_layanan_table.php
+++ database/migrations/2025_10_24_140629_create_kategori_layanan_table.php
@@ -14,6 +14,7 @@ return new class extends Migration
         Schema::create('kategori_layanan', function (Blueprint $table) {
             $table->id();
             $table->string('nama_kategori', 100);
+            $table->string('slug')->unique();
             $table->text('deskripsi')->nullable();
             $table->timestamps();
         });
