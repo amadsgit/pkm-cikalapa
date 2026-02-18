7ce516aaa312cf80a6fa91622fdb3a07d5798b83
Mamad Ahmad <mamadahmadfnc@gmail.com>
2026-02-18 21:56:36 +0700

    update seeder database awal

--- database/seeders/PegawaiSeeder.php
+++ database/seeders/PegawaiSeeder.php
@@ -9,29 +9,48 @@ class PegawaiSeeder extends Seeder
 {
     public function run(): void
     {
+
         DB::table('pegawai')->insert([
 
             [
+
                 'id' => 1,
                 'foto' => 'pegawai/foto/default.png',
 
                 'nama_pegawai' => 'Wawan Supandi,SKM',
                 'nip' => '198809052020121005',
+
                 'tempat_lahir' => 'subang',
                 'tanggal_lahir' => '1975-02-12',
+
                 'jenis_kelamin' => 'Laki-laki',
 
                 'jabatan' => 'Kepala Puskesmas',
                 'pangkat' => 'Pembina',
                 'golongan' => 'III/b',
 
+                'tmt_sk_jabatan' => null,
+                'sk_jabatan' => null,
+
+                'tmt_sk_pangkat_golongan' => null,
+                'sk_pangkat_golongan' => null,
+
                 'status_kepegawaian' => 'PNS',
+
                 'tgl_mulai_kerja' => '2020-01-05',
 
+                'str' => null,
+                'tgl_akhir_str' => null,
+
+                'sip' => null,
+                'tgl_akhir_sip' => null,
+
                 'pendidikan_terakhir' => 'S1',
+
                 'jurusan' => 'Kesehatan Masyarakat',
 
                 'hp' => '08213132312',
+
                 'alamat' => 'subang',
 
                 'created_at' => now(),
@@ -40,22 +59,42 @@ class PegawaiSeeder extends Seeder
             ],
 
             [
+
                 'id' => 2,
 
+                'foto' => null,
+
                 'nama_pegawai' => 'Dedi Ruhandi,SKM.,MM',
+
                 'nip' => '198112242010012014',
+
                 'tempat_lahir' => 'Subang',
+
                 'tanggal_lahir' => '2025-10-07',
 
                 'jenis_kelamin' => 'Laki-laki',
+
                 'jabatan' => 'Kasubag TU',
 
                 'pangkat' => 'Pembina',
+
                 'golongan' => 'IV/a',
+
+                'tmt_sk_jabatan' => null,
+                'sk_jabatan' => null,
+
+                'tmt_sk_pangkat_golongan' => null,
+                'sk_pangkat_golongan' => null,
 
                 'status_kepegawaian' => 'PNS',
 
                 'tgl_mulai_kerja' => '2022-12-14',
+
+                'str' => null,
+                'tgl_akhir_str' => null,
+
+                'sip' => null,
+                'tgl_akhir_sip' => null,
 
                 'pendidikan_terakhir' => 'S2',
 
@@ -71,5 +110,6 @@ class PegawaiSeeder extends Seeder
             ],
 
         ]);
+
     }
 }
